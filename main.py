2e0827fd911f2739cdf4db5d7be9025712739436
keren_or <57459738+kerenor557100@users.noreply.github.com>
2021-10-19 11:36:11 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,7 @@ user = "my user id"
 
 
 def some_action(post):
-    """Here you might want to do something with each post. E.g. grab the
-    post's message (post['message']) or the post's picture (post['picture']).
-    In this implementation we just print the post's created time.
-    """
+  
     print(post)
 
 
